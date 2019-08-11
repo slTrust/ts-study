134a8c52194a6e3bf4cb506efb9fc69672cf860c
huangjiaxi <trustfor@sina.cn>
2019-08-11 21:40:50 +0800

    vue-cli3 生成 ts应用

---
+++ ts010/vue-ts-demo/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    props 值为name:{{name}}
+    Hi TS vue <br>
+    mixins 值为 ： {{mixinValue}} <br>
+    data里的msg: {{msg}}  <br>
+    <button @click="fn">点我</button> <br>
+    为 HelloWorld 组件传递值 hjx <br>
+    <hello-world name="hjx"></hello-world>
+  </div>
+</template>
+
+<script lang="ts">
+/*
+// 原来的写法
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+@Component({
+  components:{
+    HelloWorld
+  }
+})
+export default class App extends Vue {
+  // 原来的 data里的数据
+  msg = 123
+
+  //原来  method 里的函数
+  fn () {
+    alert('hello: ' + this.msg)
+  }
+}
+*/
+
+// 使用 mixins
+import { Component, Vue , Mixins } from 'vue-property-decorator';
+import { MyMixin } from './mixins/mixin-one';
+import HelloWorld from './components/HelloWorld.vue';
+@Component({
+  components:{
+    HelloWorld
+  }
+})
+export default class App extends Mixins(MyMixin) {
+  // 原来的 data里的数据
+  msg = 123
+
+  //原来  method 里的函数
+  fn () {
+    alert('hello: ' + this.msg)
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: red;
+}
+</style>
---
+++ ts010/vue-ts-demo/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    我接受到的 props name 为 ：{{name}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  props:{
+    name:String
+  }
+})
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
